0873ce34ce121f91d47a9ac44ba09dbdee7aa7a0
Laura <lauralewisdev@gmail.com>
2023-11-14 16:37:04 -0500

    fix: add size_t and other int types to the global namespace (#3)
    * fix: add size_t and other int types to the global namespace
    
    * feat: move int types to dedicated header

--- MDLParser.h
+++ MDLParser.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <cstdint>
-
 #include "source/Structs.h"
 
 class VVD
---
+++ source/Ints.h
@@ -0,0 +1,12 @@
+#include <cstddef>
+#include <cstdint>
+
+using std::size_t;
+
+using std::int8_t;
+using std::int16_t;
+using std::int32_t;
+
+using std::uint8_t;
+using std::uint16_t;
+using std::uint32_t;
